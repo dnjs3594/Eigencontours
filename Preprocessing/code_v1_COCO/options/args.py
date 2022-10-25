0efb0fdf09bc9cc5e5b34517df55e49c47ea6e1a
Wonhui Park <62027647+dnjs3594@users.noreply.github.com>
2022-10-25 10:17:42 +0900

    Update args.py

--- Preprocessing/code_v1_COCO/options/args.py
+++ Preprocessing/code_v1_COCO/options/args.py
@@ -3,9 +3,9 @@ import argparse
 def parse_args(cfg):
     parser = argparse.ArgumentParser(description='Hello')
     parser.add_argument('--mode', default='encoding', help='mode')
-    parser.add_argument('--node_num', default=360, help='node_num')
-    parser.add_argument('--dim', default=36, help='dim')
-    parser.add_argument('--display', default=False, help='visualization')
+    parser.add_argument('--node_num', default=360, type=int help='node_num')
+    parser.add_argument('--dim', default=36, type=int, help='dim')
+    parser.add_argument('--display', default=False, type=bool, help='visualization')
 
     args = parser.parse_args()
     cfg = args_to_config(cfg, args)
